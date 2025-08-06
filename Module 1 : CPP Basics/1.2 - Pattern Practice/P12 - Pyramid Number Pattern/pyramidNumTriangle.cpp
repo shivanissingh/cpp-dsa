a3046630c9bc762272fda423dad37fb46bf6bb69
shivanissingh <shivanissingh01@gmail.com>
2025-08-06 13:42:40 +0530

    CPP DSA Day 4 : Module 1 - P12 : Pyramid Number Triangle Pattern (Video 4)

---
+++ Module 1 : CPP Basics/1.2 - Pattern Practice/P12 - Pyramid Number Pattern/pyramidNumTriangle.cpp
@@ -0,0 +1,40 @@
+// Triangle Pattern 12 : Pyramid Number Pattern
+
+#include <iostream>
+using namespace std ;
+
+int main() {
+
+    // Define Number of lines or rows
+    int n = 9 ; 
+
+    cout << "\nPyramid Pattern for Numbers :\n";
+    cout << "___________________________________________\n\n";
+    cout << "For n = " << n << " \n\n";
+
+    // Outer Loop = Number of lines or rows = n
+    for(int i = 0 ; i < n ; i++) {
+
+        // Inner Loop 1 : Spaces - number of spaces in each line = n - i+1
+        for(int j = 0 ; j < n - i + 1 ; j++) {
+            cout << " " ; // Print Spaces
+        }
+
+        // Define Starting Number 
+        int num = 1 ;
+        // Inner Loop 2 : Numbers - number of characters to print in each row = i+1
+        for(int j = 0 ; j < i + 1 ; j++) {
+            cout << num ; // Print Number
+            num++ ; // Increment Number
+        }
+
+        // Inner Loop 3 : Reverse Numbers - number of character to print in each row = i
+        for(int j = i ; j > 0 ; j--) {
+            cout << j ; // Print Reverse Number
+        }
+
+        cout << endl ; // Move to the Next Line
+    } 
+
+    return 0 ;
+}
