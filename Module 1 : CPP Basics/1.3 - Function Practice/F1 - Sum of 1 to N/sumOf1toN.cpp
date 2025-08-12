d38f18fe3dfc74d7fb9f7cde886e733f9155f34b
shivanissingh <shivanissingh01@gmail.com>
2025-08-12 07:29:46 +0530

    CPP DSA Day 10 : Module 1 - F1 : Function Practice 1 - Sum of 1 to N (Video 5)

---
+++ Module 1 : CPP Basics/1.3 - Function Practice/F1 - Sum of 1 to N/sumOf1toN.cpp
@@ -0,0 +1,45 @@
+// Function Practice 1 - Sum of 1 to N numbers
+
+#include <iostream>
+using namespace std;
+
+// Function calculate Sum of 1 to N numbers
+int ownApproach(int n) {
+
+    cout << "#     Own Approach - Using Formula     #" << endl ;
+    cout << "For n = " << n << " \n\n";
+    
+    // Calculate sum of 1 to n numbers
+    int sum = (n * (n + 1)) / 2;
+
+    return sum ;
+}
+
+int approach2(int n) {
+    cout << "\n#     Approach 2 - Using Loop          #" << endl ;
+    cout << "For n = " << n << " \n\n";
+
+    // Define a variable to store sum
+    int sum = 0 ;
+
+    // Loop from 1 to n
+    for(int i = 1 ; i <=n ; i++) {
+        sum = sum + i ;
+    }
+
+    return sum ;
+
+}
+
+int main() {
+
+    cout << "\nFunction Practice 1 - Sum of 1 to N numbers :\n";
+    cout << "___________________________________________\n\n";
+
+    // Invoke Function with n=10
+    cout << ownApproach(10) << endl ;
+    cout << approach2(10) << endl ;
+
+
+    return 0 ;
+}
